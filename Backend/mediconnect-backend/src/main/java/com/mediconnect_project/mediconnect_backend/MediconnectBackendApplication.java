4562a877dd0fd1c02734960009a9888aeb707fc6
Chathunga02 <130090506+Chathunga02@users.noreply.github.com>
2026-02-05 16:43:16 +0530

    initial commit

---
+++ Backend/mediconnect-backend/src/main/java/com/mediconnect_project/mediconnect_backend/MediconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediconnect_project.mediconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediconnectBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/mediconnect-backend/src/test/java/com/mediconnect_project/mediconnect_backend/MediconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediconnect_project.mediconnect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
